65460cacd264563ae84617154b021df1cad8b0f3
kouider ayad walid <kouiderayadwalid@gmail.com>
2026-06-09 10:12:24 +0200

    feat(pfleger): add AllCaregiverController with CRUD logic

---
+++ src/main/java/de/hitec/nhplus/controller/AllCaregiverController.java
@@ -0,0 +1,151 @@
+package de.hitec.nhplus.controller;
+
+import de.hitec.nhplus.datastorage.CaregiverDao;
+import de.hitec.nhplus.datastorage.DaoFactory;
+import de.hitec.nhplus.model.Caregiver;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import java.sql.SQLException;
+
+
+
+public class AllCaregiverController {
+
+    @FXML private TableView<Caregiver> tableView;
+    @FXML private TableColumn<Caregiver, Long> columnId;
+    @FXML private TableColumn<Caregiver, String> columnFirstName;
+    @FXML private TableColumn<Caregiver, String> columnSurname;
+    @FXML private TableColumn<Caregiver, String> columnPersonnelNumber;
+    @FXML private TableColumn<Caregiver, String> columnQualification;
+    @FXML private Button buttonDelete;
+    @FXML private Button buttonAdd;
+    @FXML private TextField textFieldFirstName;
+    @FXML private TextField textFieldSurname;
+    @FXML private TextField textFieldPersonnelNumber;
+    @FXML private TextField textFieldQualification;
+
+    private final ObservableList<Caregiver> caregivers = FXCollections.observableArrayList();
+    private CaregiverDao dao;
+
+    public void initialize() {
+        this.readAllAndShowInTableView();
+
+        this.columnId.setCellValueFactory(new PropertyValueFactory<>("cid"));
+
+        this.columnFirstName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        this.columnFirstName.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        this.columnSurname.setCellValueFactory(new PropertyValueFactory<>("surname"));
+        this.columnSurname.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        this.columnPersonnelNumber.setCellValueFactory(new PropertyValueFactory<>("personnelNumber"));
+        this.columnPersonnelNumber.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        this.columnQualification.setCellValueFactory(new PropertyValueFactory<>("qualification"));
+        this.columnQualification.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        this.tableView.setItems(this.caregivers);
+
+        this.buttonDelete.setDisable(true);
+        this.tableView.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> this.buttonDelete.setDisable(newValue == null)
+        );
+
+        this.buttonAdd.setDisable(true);
+        this.textFieldFirstName.textProperty().addListener((obs, old, newVal) -> buttonAdd.setDisable(!areInputDataValid()));
+        this.textFieldSurname.textProperty().addListener((obs, old, newVal) -> buttonAdd.setDisable(!areInputDataValid()));
+        this.textFieldPersonnelNumber.textProperty().addListener((obs, old, newVal) -> buttonAdd.setDisable(!areInputDataValid()));
+        this.textFieldQualification.textProperty().addListener((obs, old, newVal) -> buttonAdd.setDisable(!areInputDataValid()));
+    }
+
+    private void readAllAndShowInTableView() {
+        this.caregivers.clear();
+        this.dao = DaoFactory.getDaoFactory().createCaregiverDao();
+        try {
+            this.caregivers.addAll(this.dao.readAll());
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void handleAdd() {
+        String firstName = this.textFieldFirstName.getText();
+        String surname = this.textFieldSurname.getText();
+        String personnelNumber = this.textFieldPersonnelNumber.getText();
+        String qualification = this.textFieldQualification.getText();
+        try {
+            this.dao.create(new Caregiver(firstName, surname, personnelNumber, qualification));
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        readAllAndShowInTableView();
+        clearTextfields();
+    }
+
+    @FXML
+    public void handleDelete() {
+        Caregiver selectedItem = this.tableView.getSelectionModel().getSelectedItem();
+        if (selectedItem != null) {
+            try {
+                this.dao.deleteById(selectedItem.getCid());
+                this.tableView.getItems().remove(selectedItem);
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    public void handleOnEditFirstName(TableColumn.CellEditEvent<Caregiver, String> event) {
+        event.getRowValue().setFirstName(event.getNewValue());
+        doUpdate(event);
+    }
+
+    @FXML
+    public void handleOnEditSurname(TableColumn.CellEditEvent<Caregiver, String> event) {
+        event.getRowValue().setSurname(event.getNewValue());
+        doUpdate(event);
+    }
+
+    @FXML
+    public void handleOnEditPersonnelNumber(TableColumn.CellEditEvent<Caregiver, String> event) {
+        event.getRowValue().setPersonnelNumber(event.getNewValue());
+        doUpdate(event);
+    }
+
+    @FXML
+    public void handleOnEditQualification(TableColumn.CellEditEvent<Caregiver, String> event) {
+        event.getRowValue().setQualification(event.getNewValue());
+        doUpdate(event);
+    }
+
+    private void doUpdate(TableColumn.CellEditEvent<Caregiver, String> event) {
+        try {
+            this.dao.update(event.getRowValue());
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private void clearTextfields() {
+        this.textFieldFirstName.clear();
+        this.textFieldSurname.clear();
+        this.textFieldPersonnelNumber.clear();
+        this.textFieldQualification.clear();
+    }
+
+    private boolean areInputDataValid() {
+        return !this.textFieldFirstName.getText().isBlank() &&
+                !this.textFieldSurname.getText().isBlank() &&
+                !this.textFieldPersonnelNumber.getText().isBlank() &&
+                !this.textFieldQualification.getText().isBlank();
+    }
+
+
+
+}
